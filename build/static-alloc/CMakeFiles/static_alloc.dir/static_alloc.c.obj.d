2e3f08416702e67e893387ca693edf49cd442dd3
Ignas207 <ignasbraz20@gmail.com>
2023-05-25 19:33:43 +0300

    removed build

--- build/static-alloc/CMakeFiles/static_alloc.dir/static_alloc.c.obj.d
+++
@@ -1,3 +0,0 @@
-static-alloc/CMakeFiles/static_alloc.dir/static_alloc.c.obj: \
- D:\Programing\c\static-alloc\static-alloc\static_alloc.c \
- D:\Programing\c\static-alloc\static-alloc\static_alloc.h
